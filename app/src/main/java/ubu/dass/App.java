d86f8a9f4b317bc1ffb31db4fc839dc2e988a3ab
clopezno <clopezno@ubu.es>
2023-06-02 19:16:43 +0200

    Maven archeotyp quickstart

---
+++ app/src/main/java/ubu/dass/App.java
@@ -0,0 +1,13 @@
+package ubu.dass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
